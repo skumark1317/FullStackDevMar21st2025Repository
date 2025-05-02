e3d9c3ffb0eb6e0fbb05b7daa76460e857227f46
skumar1317 <skumark1317@gmail.com>
2025-05-02 19:59:24 +0530

    pattran match

---
+++ AccessSpecifier/AccessSpecifier/src/test/java/p3/Pattran.java
@@ -0,0 +1,16 @@
+package p3;
+
+public class Pattran {
+    public static void main(String[] args)
+    {
+        for(int i=1;i<5;i++)
+        {
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print(" 5 ");
+            }
+            System.out.println();
+        }
+
+    }
+}
