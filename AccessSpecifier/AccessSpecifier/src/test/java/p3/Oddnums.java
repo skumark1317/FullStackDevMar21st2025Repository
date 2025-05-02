eaea6f9239bdbce2d4215ccf849fca496ed78654
skumar1317 <skumark1317@gmail.com>
2025-05-02 12:39:42 +0530

    sample3 program

---
+++ AccessSpecifier/AccessSpecifier/src/test/java/p3/Oddnums.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class Oddnums {
+    public static void main(String[] args) {
+        for(int i=1;i<50;i++)
+        {
+            if(i%2==1)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
